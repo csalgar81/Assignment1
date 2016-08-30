a05509116e3c7db228db0b1049cdd6b7727c930f
CARLOS-PC <carlos.salgar1@my.jcu.edu.au>
2016-08-30 22:42:24 +1000

    ask for user choice

--- shopping_list.py
+++ shopping_list.py
@@ -1,22 +1,26 @@
 def main():
-
+    request_pool = ['R','C','A','M','Q']
     print("Shopping List v1 - Carlos Salgar")
     print("R - List Required Items\nC - List Completed Items\nA - Add new item\nM - mark an item as completed\nQ - Quit")
     load_items_file()
+    user_request = input(">>> ").upper()
+    while user_request not in request_pool:
+        print("Not valid")
+        user_request = input(">>> ").upper()
 
 def load_items_file():
     import csv
     items_file = open("items.csv", "r")
     items_reader = csv.reader(items_file)
-    items = []
-    prices = []
-    priorities = []
+    items_names = []
+    items_prices = []
+    items_priorities = []
     items_status = []
     i = 0
     for row in items_reader:
-        items.append(row[0])
-        prices.append(row[1])
-        priorities.append(row[2])
+        items_names.append(row[0])
+        items_prices.append(row[1])
+        items_priorities.append(row[2])
         items_status.append(row[3])
 
 main()
