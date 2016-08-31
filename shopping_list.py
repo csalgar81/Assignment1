f7f597023a8cfec1edd15504248063e05cd0ec64
CARLOS-PC <carlos.salgar1@my.jcu.edu.au>
2016-08-31 22:24:37 +1000

    calculates and prints completed items

--- shopping_list.py
+++ shopping_list.py
@@ -4,13 +4,35 @@ def main():
         row_number = 0
         i = 0
         total_price=0
+        there_are_required_items = False
         for status in items_status:
             if status == 'r':
                 print("{}.  {}\t$  {:<6.2f} ({})".format(row_number, items_names[i], items_prices[i], items_priorities[i]))
                 row_number += 1
                 total_price += items_prices[i]
+                there_are_required_items = True
             i += 1
-        print("Total expected price for {} items: ${:.2f}".format(row_number+1,total_price))
+        if there_are_required_items:
+            print("Total expected price for {} items: ${:.2f}".format(row_number+1,total_price))
+        else:
+            print("There are no required itmes")
+
+    def print_completed_items():
+        row_number = 0
+        i=0
+        total_price = 0
+        there_are_completed_items = False
+        for status in items_status:
+            if status == 'c':
+                print("{}.  {}\t$  {:<6.2f} ({})".format(row_number, items_names[i], items_prices[i], items_priorities[i]))
+                row_number += 1
+                total_price += items_prices[i]
+                there_are_completed_items = True
+            i += 1
+        if there_are_completed_items:
+            print("Total expected price for {} items: ${:.2f}".format(row_number + 1, total_price))
+        else:
+            print("There are no completed items")
 
     def user_input():
         request_pool = ['R', 'C', 'A', 'M', 'Q']
@@ -42,12 +64,9 @@ def main():
         if request == 'R':
             print_required_items()
             request = user_input()
-
-
-
-
-
-
+        elif request == 'C':
+            print_completed_items()
+            request = user_input()
 
 
 main()
